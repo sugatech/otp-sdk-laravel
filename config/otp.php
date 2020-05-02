867335830e663df03646d50c5245895c70429d75
Quang Hung <ngochung.quang.1310@gmail.com>
2020-05-02 16:11:49 +0700

    [#7] Use oauth2-client-credentials-laravel lib (Update config)

--- config/otp.php
+++ config/otp.php
@@ -3,4 +3,9 @@
 return [
     'api_url' => env('OTP_API_URL'),
     'access_token' => env('OTP_ACCESS_TOKEN'),
+    'oauth' => [
+        'url' => env('OTP_OAUTH_URL', env('OTP_API_URL').'/oauth/token'),
+        'client_id' => 1,
+        'client_secret' => 'WnkfwnvPTJ0ltu1OiessL0je4YQNMu1vraEIQTZ9',
+    ],
 ];
